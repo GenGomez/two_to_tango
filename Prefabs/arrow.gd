7229b28779b1acbb0c8f29d27d6d029468b62b7c
GenGomez <gomgen02@gmail.com>
2026-05-07 10:34:43 +0200

    arrow

--- Prefabs/arrow.gd
+++ Prefabs/arrow.gd
@@ -6,10 +6,20 @@ enum dirs{
 	RIGHT
 }
 
-var direction 
+var leftTexture = preload("res://Sprites/game/arrow_button/left/arrow_button_left1.png")
+var upTexture = preload("res://Sprites/game/arrow_button/up/arrow_button_up1.png")
+var rightTexture = preload("res://Sprites/game/arrow_button/right/arrow_button_right1.png")
+
+var direction = dirs.LEFT;
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	if direction == dirs.LEFT:
+		get_node("Sprite2D").set_texture(leftTexture)
+	elif direction == dirs.UP:
+		get_node("Sprite2D").set_texture(upTexture)
+	elif direction == dirs.UP:
+		get_node("Sprite2D").set_texture(rightTexture)
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
