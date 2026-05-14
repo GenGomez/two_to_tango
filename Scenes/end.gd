4a170fc7426d8e4636ee70704fcba80a24a7c2c5
GenGomez <gomgen02@gmail.com>
2026-05-14 10:37:28 +0200

    pantalla final

---
+++ Scenes/end.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if Global.victory:
+		$Victory.text = "you win!"
+	else:
+		$Victory.text = "you lose"
+		
+	$NoteCount.text = str("Missed: " , Global.missed , "\nOkay: " , Global.okay , "\nGood: " , Global.good , "\nGreat: " , Global.great)
+	$MaxCombo.text = str(Global.combo)
+	$FinalScore.text = str(Global.score)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
